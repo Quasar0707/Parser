4cd140cbda45f12467b22a0258e35f034c9469c4
Quasar0707 <marsokolov010@gmail.com>
2024-10-28 22:09:10 +0300

    В виде функции

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
+
 # очищаем код от выбранных элементов
 def delete_div(code, tag, arg):
     # находим все указанные теги с параметрами
@@ -9,35 +10,35 @@ def delete_div(code, tag, arg):
         div.decompose()
 
 
-# получаем исходный код страницы
-inner_html_code = str(urlopen('https://thecode.media/parsing-2/').read(), 'utf-8')
+def clear_text(url):
+    # получаем исходный код страницы
+    inner_html_code = str(urlopen(url).read(), 'utf-8')
 
-# отправляем исходный код страницы на обработку в библиотеку
-inner_soup = BeautifulSoup(inner_html_code, "html.parser")
+    # отправляем исходный код страницы на обработку в библиотеку
+    inner_soup = BeautifulSoup(inner_html_code, "html.parser")
 
-title = inner_soup.find('title').text
+    # Только блок с содержанием статьи
+    inner_soup = inner_soup.find('div', {"class": 'article-content'})
 
-# Только блок с содержанием статьи
-inner_soup = inner_soup.find('div', {"class": 'article-content'})
+    # удаляем титры
+    delete_div(inner_soup, "div", {'class': "wp-block-lazyblock-titry"})
 
-# удаляем титры
-delete_div(inner_soup, "div", {'class': "wp-block-lazyblock-titry"})
+    # удаляем все возможные баннеры (код баннеров от 1 до 99)
+    for i in range(99):
+        delete_div(inner_soup, "div", {'class': "wp-block-lazyblock-banner" + str(i)})
 
-# удаляем все возможные баннеры (код баннеров от 1 до 99)
-for i in range(99):
-    delete_div(inner_soup, "div", {'class': "wp-block-lazyblock-banner" + str(i)})
+    # Удаляем боковые ссылки
+    delete_div(inner_soup, "div", {'class': "wp-block-lazyblock-link-aside"})
 
-# Удаляем боковые ссылки
-delete_div(inner_soup, "div", {'class': "wp-block-lazyblock-link-aside"})
+    # удаляем кат
+    delete_div(inner_soup, "div", {"class": "accordion"})
 
-# удаляем кат
-delete_div(inner_soup, "div", {"class": "accordion"})
+    # удаляем преформатированный код
+    delete_div(inner_soup, 'pre', '')
 
-# удаляем преформатированный код
-delete_div(inner_soup, 'pre', '')
+    # удаляем вставки с кодом
+    delete_div(inner_soup, 'code', '')
+    return inner_soup.get_text()
 
-# удаляем вставки с кодом
-delete_div(inner_soup, 'code', '')
 
-print(title)
-print(inner_soup.get_text())
+print(clear_text('https://thecode.media/parsing-2/'))
